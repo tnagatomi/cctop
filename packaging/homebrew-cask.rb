ce5f63a4ebc634c93105b6319e81cb24eebf2e46
st0012 <stan001212@gmail.com>
2026-03-24 23:37:36 +0000

    Bump version to 0.13.1

--- packaging/homebrew-cask.rb
+++ packaging/homebrew-cask.rb
@@ -7,7 +7,7 @@
 #   brew install --cask cctop
 #
 cask "cctop" do
-  version "0.13.0"
+  version "0.13.1"
 
   on_arm do
     url "https://github.com/st0012/cctop/releases/download/v#{version}/cctop-macOS-arm64.zip"
