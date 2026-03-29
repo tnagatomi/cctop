1c94eac7de6d9bcc8d6809d38d7c50b9433aa447
st0012 <stan001212@gmail.com>
2026-03-29 02:32:21 +0100

    Bump version to 0.13.2

--- menubar/CctopMenubar/Hook/HookMain.swift
+++ menubar/CctopMenubar/Hook/HookMain.swift
@@ -8,7 +8,7 @@ import Foundation
 /// Usage: cctop-hook <HookName>
 @main
 struct HookMain {
-    static let version = "0.13.1"
+    static let version = "0.13.2"
 
     static func main() {
         let args = CommandLine.arguments
